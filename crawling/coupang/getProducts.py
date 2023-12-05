282960279b0d8b4c165a3b5996f8425fdc512aa7
윤영미 <maie421@naver.com>
2023-12-05 21:03:23 +0900

    feat : 크롤링 insert query

--- crawling/coupang/getProducts.py
+++ crawling/coupang/getProducts.py
@@ -1,35 +1,60 @@
 from stringgetter import getPageString
 from bs4 import BeautifulSoup
+import re
+
 
 def getProducts(string):
+    results_list = []
     bsObj = BeautifulSoup(string, "html.parser")
-    ul = bsObj.find("ul", {"id":"productList"})  #아이템 리스트부분 추출
-    lis = ul.findAll("li", {"class":"baby-product renew-badge"}) #각 아이템 추출
+    ul = bsObj.find("ul", {"id": "productList"})  # 아이템 리스트부분 추출
+    lis = ul.findAll("li", {"class": "baby-product renew-badge"})  # 각 아이템 추출
 
     for item in lis:
-        #url
+        # url
         a = item.find("a", {"class": "baby-product-link"})
         url = a.get('href')
-        print("url:", url)
 
-        #name
-        div_name = item.find("div", {"class":"name"})
+        # 정규 표현식을 사용하여 productId와 categoryId 추출
+        product_id_match = re.search(r'/vp/products/(\d+)', url)
+        category_id_match = re.search(r'categoryId=(\d+)', url)
+
+        # name
+        div_name = item.find("div", {"class": "name"})
         name = div_name.getText()
-        print("name:", name)
 
-        #image
-        dt_image = item.find("dt", {"class":"image"})
+        # image
+        dt_image = item.find("dt", {"class": "image"})
         image = dt_image.find("img").get('src')
-        print("image:", image)
 
-        #price
-        price = item.find("strong", {"class":"price-value"}).getText()
-        print("price:", price)
+        # price
+        price = item.find("strong", {"class": "price-value"}).getText()
 
+        # 결과를 리스트에 추가
+        results_list.append({
+            'product_id': product_id_match.group(1) if product_id_match else None,
+            'category_id': category_id_match.group(1) if category_id_match else None,
+            'name': name,
+            'image': image,
+            'price': price
+        })
     print(len(lis))
+
+    sql_statement = "INSERT INTO log.products (name, product_id, category_id, image, price) VALUES "
+
+    for result in results_list:
+        price = result['price'].replace(',', '')
+
+        values = f"('{result['name'].strip()}', {result['product_id']}, {result['category_id']}, '{result['image']}', {price})"
+        sql_statement += values + ", "
+
+    sql_statement = sql_statement.rstrip(", ")
+    print(sql_statement)
     return []
 
-for i in range(1, 6):
-    url = "https://www.coupang.com/np/categories/502483?channel=plp_C2&page=" + str(i)
-    pageString = getPageString(url)
-    print(getProducts(pageString))
+
+# for j in [502483, 502484, 502485, 502486, 502487]:
+for j in [502483]:
+    for i in range(1, 2):
+        url = "https://www.coupang.com/np/categories/" + str(j) + "?listSize=100&channel=plp_C2&page=" + str(i)
+        pageString = getPageString(url)
+        getProducts(pageString)
