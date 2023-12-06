f5752b5cc3d429307c4cc3993ff031a47a34a0a5
윤영미 <maie421@naver.com>
2023-12-06 14:40:57 +0900

    fix : test

--- crawling/coupang/getProducts.py
+++ crawling/coupang/getProducts.py
@@ -4,7 +4,8 @@ import re
 import pymysql
 import sys
 
-conn = pymysql.connect(host=f"'{sys.argv[1]}'", user=f"'{sys.argv[2]}'", password=f"'{sys.argv[3]}'", db='log', charset='utf8')
+conn = pymysql.connect(host='13.124.180.172', user='batch', password='track', db='log', charset='utf8')
+# conn = pymysql.connect(host=f"'{sys.argv[1]}'", user=f"'{sys.argv[2]}'", password=f"'{sys.argv[3]}'", db='log', charset='utf8')
 cur = conn.cursor()
 
 
