ebe4ce59956ff9609716d99cc46f5c4aa275e4b6
윤영미 <maie421@naver.com>
2023-12-10 03:13:21 +0900

    feat : 할인률 높은 상위 12개 상품

--- web/app.py
+++ web/app.py
@@ -1,15 +1,16 @@
 import sys
 from flask import Flask
-from web.dbConnect import db_connect
-from web.controller.products import Products
-
+from dbConnect import db_connect
+from controller.products import Products
+from testDbConnect import db_connect_test
 # Flask 객체 인스턴스 생성
 app = Flask(__name__)
-db_connection = db_connect(sys)
+# db_connection = db_connect(sys)
+db_connect = db_connect_test()
 
 product = Products(app, db_connect)
 
-@app.route('/')  # 접속하는 url
+@app.route('/')
 def index():
     return product.index()
 
