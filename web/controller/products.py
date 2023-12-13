df8ccdcc5cf768bbe6c8c215e0bb24e0ada52e1b
윤영미 <maie421@naver.com>
2023-12-13 17:31:01 +0900

    feat : 주석 처리

--- web/controller/products.py
+++ web/controller/products.py
@@ -22,6 +22,7 @@ class Products:
 
             df['discount_rate'] = (df.avg_price - df.price) / df.avg_price * 100
 
+            # 할인율이 높은 상품 리스트
             top_12_discounted = df.nlargest(12, 'discount_rate')
 
             return render_template('index.html', products=top_12_discounted.to_dict('records'))
@@ -50,13 +51,16 @@ class Products:
         cursor.execute("SELECT max(high_price) as high_price, min(low_price) as low_price FROM track_price_changes.products_stats WHERE product_id = %s", pid)
         product_price_data = cursor.fetchone()
 
-        cursor.execute("SELECT high_price,low_price, created_at FROM track_price_changes.products_stats WHERE created_at >= %s AND created_at <= %s and product_id = %s", (start_date, end_date, pid))
+        cursor.execute("SELECT high_price, low_price, created_at FROM track_price_changes.products_stats WHERE created_at >= %s AND created_at <= %s and product_id = %s", (start_date, end_date, pid))
         product_stats_data = cursor.fetchall()
 
+        # 크롤링된 데이터 날짜
         date_to_index = [row[2].strftime("%Y-%m-%d") for row in product_stats_data]
-        print(date_to_index)
+        # 최근 일주일 날짜
         labels = [(datetime.now() - timedelta(days=i)).strftime("%Y-%m-%d") for i in range(6, -1, -1)]
-        print(labels)
+
+        # 최근 일주일 기준 존재 하고 있지 않는 크롤링 데이터 확인
+        # 해당 상품은 품절 상태
         not_in_labels = []
         for label in labels:
             if label not in date_to_index:
@@ -65,7 +69,6 @@ class Products:
         high_price = [row[0] for row in product_stats_data]
         low_price = [row[1] for row in product_stats_data]
 
-        print(not_in_labels)
         for i in not_in_labels:
             high_price.insert(i, "")
             low_price.insert(i, "")
